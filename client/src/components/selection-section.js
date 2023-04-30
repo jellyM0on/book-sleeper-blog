eea68937c28de0684fe2f7fa1ccc35ff3076572d
jellyM0on <ylana.ong@gmail.com>
2023-04-30 15:11:14 +0800

    separate works in selection page by year if date or letter if alphabetical

--- client/src/components/selection-section.js
+++ client/src/components/selection-section.js
@@ -1,22 +1,23 @@
 import { Link } from "react-router-dom";
 
-export default function Section(props){
+export default function WorksList(props){
     const {key, data} = props; 
 
     console.log(data); 
     return(
         <div>
-            <div>
-                <p>{key}</p>
-            </div>
-            <div>
             {data.map((d, i) => {
                 return(
-                    <Link to={`./${d.title}`}> <p key={i}>{d.title}</p></Link>
+                    <div> 
+                        <p>{d.key}</p>
+                        {d.works.map((x, y) => {
+                            return(
+                                <p>{x.title}</p>
+                            )
+                        })}
+                    </div>
                 )
             })}
-            </div>
-           
         </div>
     )
 
--- client/src/components/works-page.js
+++ client/src/components/works-page.js
@@ -1,21 +1,87 @@
 import { motion as m } from 'framer-motion';
 import NavTab from './nav-tab';
-import Section from './selection-section';
 import Sort from './sort-works';
 import { useState, useEffect } from 'react';
+import WorksList from './selection-section';
 
 function Works(props){
     const {data} = props; 
 
     const [sortKey, setSortKey] = useState(''); 
 
-    const alphaSorted = data.slice().sort((a, b) => a.title > b.title ? 1 : -1);
+    const alphaSorted = () => {
+
+        const findFirst = (data) => {
+            const title = `${data.title}`; 
+            const wordArr = title.split('');
+            const letter = wordArr[0];
+            return letter; 
+        }
+
+        const sorted = data.slice().sort((a, b) => a.title > b.title ? 1 : -1);
+
+        let letters = []; 
+        sorted.forEach((d) => {
+            const letter = findFirst(d); 
+            if (!letters.find(x => (x == letter))){
+                letters.push(letter);
+            }
+        })
+       
+        let groupedArr = [];
+        letters.forEach((x) => {
+            let arr = []; 
+            let key = x; 
+            sorted.forEach((d) => {
+                const letter = findFirst(d); 
+                console.log(key); 
+                if(letter == x){
+                    arr.push(d); 
+                }
+            })
+            groupedArr.push({key: key, works: arr}); 
+        })
+
+        return groupedArr; 
+    }
+
+    const dateSorted = () => {
+
+        const sorted = data; 
+
+        const findYear = (data) => {
+            const dateObj = new Date(data.date)
+            return dateObj.getFullYear(); 
+        }
+
+        let years = []; 
+        sorted.forEach((d) => {
+            const year = findYear(d);
+            if (!years.find(x => (x == year))){
+                years.push(year);
+            }
+        })
+
+        let groupedArr = [];
+        years.forEach((x) => {
+            let arr = [];
+            let key = x; 
+            sorted.forEach((d) => {
+                const year = findYear(d); 
+                if(year == x){
+                    arr.push(d); 
+                }
+            })
+            groupedArr.push({key: key, works: arr}); 
+        })
+
+        return groupedArr; 
+    }
 
     return(
         <div class='works-page'>
             <Sort setSortKey={setSortKey}/>
-            <Section data={sortKey === 'alphabet' ? alphaSorted : data}/>
-
+            <WorksList data={sortKey === 'alphabet' ? alphaSorted() : dateSorted()}/>
         </div>
     )
 }
