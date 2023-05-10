3925a616ddf387bb2bcf8a25e9ef2ac0a96cc8d8
jellyM0on <ylana.ong@gmail.com>
2023-05-10 22:03:51 +0800

    center contact page in smaller screens

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const cors = require('cors');
-const expbhs = require('express-handlebars');
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer'); 
 
 const app = express();
