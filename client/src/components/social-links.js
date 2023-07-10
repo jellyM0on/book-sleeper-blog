a29f484b882f57998512684261a7a67f2339fc85
jellyM0on <ylana.ong@gmail.com>
2023-07-11 00:13:15 +0800

    add correct links in social links

--- client/src/components/social-links.js
+++ client/src/components/social-links.js
@@ -5,7 +5,7 @@ export default function SocialLinks(){
     return(
         <div className='social-links'>
             <a href='https://github.com/jellyM0on' target='_blank'><img src={githubicon} ></img> </a>
-            <a href='https://www.linkedin.com/' target='_blank'> <img src={linkedinicon}></img> </a>
+            <a href='https://www.linkedin.com/in/28b8b6259/' target='_blank'> <img src={linkedinicon}></img> </a>
            
         </div>
     )
